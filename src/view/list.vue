3904782bb793435d2abfcfd43e477fcb1fc0a9c9
15649012981 <15649012981@163.com>
2018-02-28 17:18:39 +0800

    xiugai

--- src/view/list.vue
+++ src/view/list.vue
@@ -27,49 +27,49 @@
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay1.jpg',
+              imgUrl: '/static/img/Jay1.jpg',
               proId: '01'
             },
             {
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay2.jpg',
+              imgUrl: '/static/img/Jay2.jpg',
               proId: '02'
             },
             {
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay3.jpg',
+              imgUrl: '/static/img/Jay3.jpg',
               proId: '03'
             },
             {
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay4.jpg',
+              imgUrl: '/static/img/Jay4.jpg',
               proId: '04'
             },
             {
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay5.jpg',
+              imgUrl: '/static/img/Jay5.jpg',
               proId: '05'
             },
             {
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay6.jpg',
+              imgUrl: '/static/img/Jay6.jpg',
               proId: '06'
             },
             {
               title: '依然范特西',
               dataTime: '2003年06月25日发布',
               worksInstr: '列表：《开不了口》、《七里香》、《你听得到》、《你好吗》、《你比从前快乐》',
-              imgUrl: './static/img/Jay3.jpg',
+              imgUrl: '/static/img/Jay3.jpg',
               proId: '07'
             }
           ]
